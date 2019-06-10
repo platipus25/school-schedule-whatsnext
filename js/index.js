16df7483a24c81cfdb6a11f002475d00662c2d87
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-06-10 14:07:48 -0700

    Use Jquery

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 
 (async () => {
-    let ky = await import("/js/ky.min.js")
 
-    let schedule_base = await ky.default("https://raw.githubusercontent.com/platipus25/whatsnext/master/examples/config_files/schedule2018-19.json").json(); // /schedule2018-19.json
-    window.inst = new whatsnext.Whatsnext(schedule_base, new Date());
+    let schedule_base = await $.getJSON("https://raw.githubusercontent.com/platipus25/whatsnext/master/examples/config_files/schedule2018-19.json"); // /schedule2018-19.json
+    let nextSchool = new whatsnext.WhatsnextStatic(schedule_base, new Date()).enumerateNextClass().start.toDate()
+    console.log(nextSchool)
+    window.inst = new whatsnext.WhatsnextSim(schedule_base, 1, nextSchool);//new Date(2019, 2, 8, 9, 30));
     console.log(inst)
+    updateFrame()
 
 })()
 
