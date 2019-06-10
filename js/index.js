da9422ee8baf6cb664ce0e5ef6b32d26cab9f411
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-06-10 14:33:28 -0700

    Implement

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-
 (async () => {
-
     let schedule_base = await $.getJSON("https://raw.githubusercontent.com/platipus25/whatsnext/master/examples/config_files/schedule2018-19.json"); // /schedule2018-19.json
     let nextSchool = new whatsnext.WhatsnextStatic(schedule_base, new Date()).enumerateNextClass().start.toDate()
     console.log(nextSchool)
@@ -10,7 +8,59 @@
 
 })()
 
-let updateFrame = () => {
-    let root = $("#container")
-    root
+let countdown = (id) => {
+    let value = inst[id]() || ""
+    let node = $( `#${ id }` )
+    let parent = node.parent()
+    
+    if(value) value = value.toHTML("number")
+    node.html(value)
+   
+    if(value){
+        parent.show()
+    }else{
+        parent.hide()
+    }
 }
+
+let className = (id) => {
+    let value = inst[id]() || ""
+    let node = $( `#${ id }` )
+    let parent = node.parent()
+    
+    if(value) value = value.class.name
+    node.text(value)
+
+    if(value){
+        parent.show()
+    }else{
+        parent.hide()
+    }
+}
+
+let percent = () => {
+    let value = inst.percent || ""
+    let node = $( "#percent" )
+    let parent = node.parent()
+
+    node.css("width", `${inst.percent}vw`)
+    node.css("transform", `translateX(calc(50vw - ${inst.percent/2}vw))`)
+    
+    if(value){
+        parent.show()
+    }else{
+        parent.hide()
+    }
+}
+
+let updateFrame = () => {
+    //let inst = window.inst
+    countdown("thisClassCountdown")
+    countdown("enumerateNextClassCountdown")
+    countdown("endOfSchoolCountdown")
+    className("thisClass")
+    className("enumerateNextClass")
+    percent()
+}
+
+setInterval(updateFrame, 1000)
